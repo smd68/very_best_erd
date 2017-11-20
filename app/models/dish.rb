179f8b3b4e6a7bddf4c232cf50766583709121d9
Raghu Betina <raghu@firstdraft.com>
2017-11-20 20:07:14 +0000

    Direct association between dish_details and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :dish,
+             :class_name => "DishDetail",
+             :foreign_key => "dish_details_id"
+
   # Indirect associations
 
   # Validations
--- app/models/dish_detail.rb
+++ app/models/dish_detail.rb
@@ -1,6 +1,11 @@
 class DishDetail < ApplicationRecord
   # Direct associations
 
+  has_many   :best_dishes,
+             :class_name => "Dish",
+             :foreign_key => "dish_details_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
