23864dc3d44490dc914e54182ad029d41142cb07
Raghu Betina <raghu@firstdraft.com>
2017-11-20 20:07:40 +0000

    Whitelist columns in Dish for ActiveAdmin

--- app/admin/dish.rb
+++ app/admin/dish.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Dish do
 
+ permit_params :dish_details_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
