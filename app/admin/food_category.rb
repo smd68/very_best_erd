e35a91388d778127043a995dc3c37994fc9ca8e4
Raghu Betina <raghu@firstdraft.com>
2017-11-20 20:07:42 +0000

    Whitelist columns in FoodCategory for ActiveAdmin

--- app/admin/food_category.rb
+++ app/admin/food_category.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FoodCategory do
 
+ permit_params :category_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
